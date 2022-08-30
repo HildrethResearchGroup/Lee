e6294bda03395319caafb0a11a15707f55e6b48d
Cole Awabdy <colekawabdy@mines.edu>
2022-08-30 12:01:57 -0600

    Initial Commit

---
+++ Lee/LeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeeApp.swift
+//  Lee
+//
+//  Created by Mines Student on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
