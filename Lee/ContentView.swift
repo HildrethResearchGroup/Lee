651d3261da39eda85395db283d68d232beb03201
DavitaBird <davsbird@gmail.com>
2022-09-13 15:57:24 -0600

    adding file chooser capacity

--- Lee/ContentView.swift
+++ Lee/ContentView.swift
@@ -14,12 +14,40 @@ struct ContentView: View {
     
     var body: some View {
         HStack {
+            Button(action: loadFile) {
+                Text("Load File")
+            }
             TextField("Input Integer", value: $dataModel.integerToPass, format: .number)
                 .disabled(dataModel.scriptIsRunning)    // Notice that you can dynamically disable views and more with Swift UI
                 .frame(width: 100)
                 .padding()
         }
         
+    }
+}
+
+func loadFile(){
+    let dialog = NSOpenPanel();
+
+    dialog.title                   = "Choose a file| Our Code World";
+    dialog.showsResizeIndicator    = true;
+    dialog.showsHiddenFiles        = false;
+    dialog.allowsMultipleSelection = false;
+    dialog.canChooseDirectories = false;
+
+    if (dialog.runModal() ==  NSApplication.ModalResponse.OK) {
+        let result = dialog.url // Pathname of the file
+
+        if (result != nil) {
+            let path: String = result!.path
+            
+            // path contains the file path e.g
+            // /Users/ourcodeworld/Desktop/file.txt
+        }
+        
+    } else {
+        // User clicked on "Cancel"
+        return
     }
 }
 
