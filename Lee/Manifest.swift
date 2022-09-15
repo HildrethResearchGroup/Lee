3f732a1ba7d095c252b4ee7f2a6c24a6a51309fe
Cole Awabdy <colekawabdy@mines.edu>
2022-09-15 11:11:24 -0600

    Add data types for manifest

---
+++ Lee/Manifest.swift
@@ -0,0 +1,28 @@
+//
+//  Manifest.swift
+//  Lee
+//
+//  Created by Mines Student on 9/15/22.
+//
+
+import Foundation
+
+enum Runner {
+    case python, matlab
+}
+
+enum DataType {
+    case path, string, int
+}
+
+struct Parameter {
+    let name: String;
+    let type: DataType;
+    let comment: String?;
+}
+
+struct Manifest {
+    let runner: Runner;
+    let inputs: [Parameter];
+    let outputs: [Parameter];
+}
