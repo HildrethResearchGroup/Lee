59a06cba6100147b1d410ee0ec56ba535ba65ecf
IsaFernandes22 <isabellafernandesde@mines.edu>
2022-09-16 16:24:55 -0600

    Deleted python sample script and created files for view, viewmodel, and data model. The select file button should now correctly pass in the manifest filepath to the model (LeeDataModel).

--- Lee/ContentView.swift
+++ Lee/ContentView.swift
@@ -7,53 +7,57 @@
 
 import SwiftUI
 
+//VIEW
 struct ContentView: View {
     // Handy charge on which Property Wrapper t use
     // https://swiftuipropertywrappers.com
-    @ObservedObject var dataModel: PythonExectuor
+    @ObservedObject var viewModel: LeeApp
     
     var body: some View {
         HStack {
             Button(action: loadFile) {
                 Text("Load File")
             }
-            TextField("Input Integer", value: $dataModel.integerToPass, format: .number)
-                .disabled(dataModel.scriptIsRunning)    // Notice that you can dynamically disable views and more with Swift UI
-                .frame(width: 100)
-                .padding()
         }
         
     }
-}
-
-func loadFile(){
-    let dialog = NSOpenPanel();
-
-    dialog.title                   = "Choose a file| Our Code World";
-    dialog.showsResizeIndicator    = true;
-    dialog.showsHiddenFiles        = false;
-    dialog.allowsMultipleSelection = false;
-    dialog.canChooseDirectories = false;
-
-    if (dialog.runModal() ==  NSApplication.ModalResponse.OK) {
-        let result = dialog.url // Pathname of the file
-
-        if (result != nil) {
-            let path: String = result!.path
+    
+    func loadFile(){
+        let dialog = NSOpenPanel();
+        
+        dialog.title                   = "Choose a file| Our Code World";
+        dialog.showsResizeIndicator    = true;
+        dialog.showsHiddenFiles        = false;
+        dialog.allowsMultipleSelection = false;
+        dialog.canChooseDirectories = false;
+        
+        if (dialog.runModal() ==  NSApplication.ModalResponse.OK) {
+            let result = dialog.url // Pathname of the file
             
-            // path contains the file path e.g
-            // /Users/ourcodeworld/Desktop/file.txt
+            if (result != nil) {
+                let path: String = result!.path
+                viewModel.saveFile(path)
+                // path contains the file path e.g
+                // /Users/ourcodeworld/Desktop/file.txt
+                //saveFile(path)
+                
+            }
+        } else {
+            // User clicked on "Cancel"
+            return
         }
         
-    } else {
-        // User clicked on "Cancel"
-        return
     }
 }
+    
+    
 
+
+/*
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        let dataModel = PythonExectuor()
-        ContentView(dataModel: dataModel)
+        let dataModel = LeeApp()
+        ContentView(viewModel: dataModel)
     }
 }
+*/
--- Lee/LeeApp.swift
+++ Lee/LeeApp.swift
@@ -5,44 +5,19 @@
 //  Created by Mines Student on 8/30/22.
 //
 
-import SwiftUI
+import Foundation
 
-@main
-struct LeeApp: App {
-    @StateObject var dataModel = PythonExectuor()
+//this is the VIEWMODEL
+//needs to be published and content view needs to subscribe to it
+
+class LeeApp: ObservableObject {
+    @Published private var model = LeeDataModel()
     
+   
+    //intent function for when user chooses manifest file through view
     
-    var body: some Scene {
-        WindowGroup {
-            ContentView(dataModel: dataModel)
-                .frame(minWidth: 400, minHeight: 300)
-                .toolbar {  // Adding a tool bar button is easy
-                    playButtonView
-                    
-                }
-        }
-    }
-    
-    // You can refactor complex views as properties or functions
-    var playButtonView: some View {
-        Button(action: runScript, label: {
-            // One way to switch between types of views is to use Group view to encapsulate logic.
-            Group {
-                if dataModel.scriptIsRunning {
-                    ProgressView()
-                } else {
-                    Image(systemName: "play.fill")
-                }
-            }
-        }).disabled(dataModel.scriptIsRunning)
-        .help(dataModel.scriptIsRunning ? Text("Script is Running") : Text("Run Script"))
-        // adding .help to a view creates a tool tip.  Notice how we can use the '?' ternary operator to switch between states.
-    }
-    
-    func runScript() {
-        Task {
-            await dataModel.runScript()
-        }
+    func saveFile(_ filename: String){
+        model.manifestFilename = filename
     }
     
 
