3ec3679200ef9b888e3dafc656339e87dab57a9e
Cole Awabdy <colekawabdy@mines.edu>
2022-09-25 13:45:14 -0600

    Modified manifest parser so it throws errors

--- Lee/ContentView.swift
+++ Lee/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
             Button(action: loadFile) {
                 Text("Load File")
             }
+            
         }
         
     }
--- Lee/Manifest.swift
+++ Lee/Manifest.swift
@@ -7,53 +7,62 @@
 
 import Foundation
 
-enum Runner: String, Codable {
-    case python, matlab
-}
-
-enum DataType: String, Codable {
-    case path
-    case string
-    case int
-}
-
-struct Program: Codable {
-    let runner: Runner
-}
-
-struct Parameter: Codable, Equatable {
-    let name: String
-    let type: DataType
-    let comment: String?
-}
-
-struct Output: Codable, Equatable {
-    let files: [String]
+// Custom errors for manifest parser
+enum ManifestParseError: Error {
+    case badEncoding
+    case decodingFailure(DecodingError)
 }
 
 struct Manifest: Codable {
-    let program: Program
-    let inputs: [Parameter]
-    let output: Output
-    static func parseFromString(source: String) -> Manifest? {
-        var manifest: Manifest?
-        do {
-            // Only parse the source if the source can be converted to data
-            if let manifestData = source.data(using: .utf8) {
-                let jsonDecoder = JSONDecoder()
-                // Parse the manifest by splitting it into the Program, Inputs, and Outputs section.
-                /*
-                 Program - Specifies the program to run the script
-                 Inputs - This will specify the script to run with the program specified in Program.
-                 Additionally, this specifies any arguments to pass the script, such as min, max, and time out values.
-                 Outputs - This specifies the file to output the script's output too after it finishes running.
-                 It writes the values written to stdout to the output files in the order provided for both the script and the output file.
-                 */
-                manifest = try jsonDecoder.decode(Manifest.self, from: manifestData)
-            }
-        } catch {
-            print(error)
+    // Program section representation
+    struct Program: Codable {
+        // Possible programs to execute scripts
+        enum Runner: String, Codable {
+            case python
+            case matlab
         }
-        return manifest
+        
+        let runner: Runner
+        let entry: String // Main file for script
+        let version: String? // Optional runner version
     }
+    
+    // Representation of a single script input
+    struct Input: Codable {
+        // Datatype of input
+        enum DataType: String, Codable {
+            case path
+            case string
+            case int
+            case float
+        }
+        
+        let name: String
+        let type: DataType
+        let comment: String?
+    }
+    
+    // Reusable decoder for parsing from string
+    private static let decoder = JSONDecoder()
+    
+    // Attempt to parse a manifest from a string source
+    static func fromString(source: String) throws -> Manifest {
+        // Proceed only if the string can be converted to data
+        if let manifestData = source.data(using: .utf8) {
+            // Attempt to decode into manifest, throw error on failure
+            do {
+                return try decoder.decode(self, from: manifestData)
+            } catch let error as DecodingError {
+                // Rethrow as ManifestParseError
+                throw ManifestParseError.decodingFailure(error)
+            }
+        } else {
+            // Failed to convert to data, string had bad encoding
+            throw ManifestParseError.badEncoding
+        }
+    }
+    
+    let program: Program
+    let inputs: [Input]
+    let output: [String]
 }
