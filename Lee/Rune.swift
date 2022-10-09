09338f31bdff51cfa40e012e014105e80481a21c
Kai Hoshijo <kaihoshijo003@gmail.com>
2022-10-09 12:18:04 -0600

    Wrote the code to pass the tests for verifiying commands

--- Lee/Rune.swift
+++ Lee/Rune.swift
@@ -8,27 +8,22 @@
 import Foundation
 
 struct Rune {
-    static func isValidRuneStart(command: String) -> Bool {
-       return false
-    }
-    
-    static func isValidRuneEnd(command: String) -> Bool {
-        return false
-    }
-    
-    static func isValidRuneFile(command: String) -> Bool {
-        return false
-    }
-    
-    static func isValidRuneError(command: String) -> Bool {
-        return false
-    }
-    
     static func isValidRuneSchema(command: String) -> Bool {
         return false
     }
-    
     static func isValidRuneCommand(command: String) -> Bool {
+        return RuneCommands(rawValue: command) != nil
+    }
+    static func isValidRuneStart(command: String) -> Bool {
+       return false
+    }
+    static func isValidRuneEnd(command: String) -> Bool {
+        return false
+    }
+    static func isValidRuneFile(command: String) -> Bool {
+        return false
+    }
+    static func isValidRuneError(command: String) -> Bool {
         return false
     }
 }
---
+++ Lee/RuneCommands.swift
@@ -0,0 +1,15 @@
+//
+//  RuneCommands.swift
+//  Lee
+//
+//  Created by Mines Student on 10/9/22.
+//
+
+import Foundation
+
+enum RuneCommands: String, Codable {
+    case START
+    case END
+    case FILE
+    case ERROR
+}
