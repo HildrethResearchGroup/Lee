d68b374921ed0a535cca0f5a7fb4ac2123d2c231
Cole Awabdy <colekawabdy@mines.edu>
2022-10-20 14:01:13 -0600

    Some data structures for storing settings

---
+++ Lee/Views/SettingsView.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsView.swift
+//  Lee
+//
+//  Created by Mines Student on 10/18/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct SettingsView: View {
+    public enum Tabs: Hashable {
+        case runner
+    }
+    
+    var body: some View {
+        TabView {
+            RunnerSettingsView(runners: viewModel.$settings)
+                .tabItem {
+                    Label("Runner", systemImage: "play.circle.fill")
+                }.tag(Tabs.runner)
+        }.frame(width: 400, height: 300, alignment: .center)
+    }
+    
+    @StateObject
+    var viewModel: SettingsViewModel
+}
