265daec65dd0d5fe8b2f6da8cf55e255963a0f96
Cole Awabdy <colekawabdy@mines.edu>
2022-11-02 19:03:13 -0600

    Executable path is editable

--- Lee/Views/RunnerEditView.swift
+++ Lee/Views/RunnerEditView.swift
@@ -9,19 +9,53 @@ import Foundation
 import SwiftUI
 
 struct RunnerEditView: View {
+    private func saveName() {
+        selection.removeAll()
+        DispatchQueue.main.async {
+            viewModel.renameRunnerVersion(prevName: currentlyEditing!, newName: versionNameEdit)
+            currentlyEditing = nil
+        }
+    }
+    
+    private func selectionChanged(selection: Set<String>) {
+        if selection.count == 1 {
+            executableEdit = viewModel.runnerVersions[viewModel.selectedRunner!][selection.first!]!
+        } else {
+            executableEdit = ""
+        }
+    }
+    
     let versions: [String]
     
     @ObservedObject var viewModel: SettingsViewModel
     
     @State private var selection = Set<String>()
     @State private var executableEdit: String = ""
+    @State private var versionNameEdit: String = ""
+    @State private var currentlyEditing: String?
+    @FocusState private var editFocus: Bool
     
     var body: some View {
         VStack {
             List(versions, id: \.self, selection: $selection) { version in
-                Text(version)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
+                if version != currentlyEditing {
+                    Text(version)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .onTapGesture(count: 2, perform: {
+                            currentlyEditing = version
+                            versionNameEdit = version
+                        })
+                } else {
+                    TextField("", text: $versionNameEdit)
+                        .onSubmit(saveName)
+                        .focused($editFocus)
+                        .task {
+                            selection.removeAll()
+                            selection.insert(version)
+                            editFocus = true
+                        }
+                }
+            }.onChange(of: selection, perform: selectionChanged)
             ListEditView(plus: {
                 viewModel.addRunnerVersion(name: "version")
             }, minus: {
@@ -30,12 +64,18 @@ struct RunnerEditView: View {
             HStack {
                 Text("Path: ")
                 Spacer()
-                TextField("", text: $executableEdit)
+                TextField("", text: $executableEdit, onEditingChanged: { changed in
+                    if changed {
+                        DispatchQueue.main.async {
+                            viewModel.setRunnerVersionExecutable(version: selection.first!, path: executableEdit)
+                        }
+                    }
+                })
                 Button("Browse") {
                     
                 }
             }
-            .disabled(selection.count != 1)
+            .disabled(selection.count != 1 && currentlyEditing == nil)
             Spacer(minLength: 48)
         }
     }
