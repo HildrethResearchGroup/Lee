05dd34966eedf608315b22bb6fa5a543e5e4fbc6
Cole Awabdy <colekawabdy@mines.edu>
2022-11-13 09:45:02 -0700

    Switch away from UUIDs

--- Lee/Settings/RunnerListView.swift
+++ Lee/Settings/RunnerListView.swift
@@ -10,16 +10,44 @@ import SwiftUI
 
 struct RunnerListView: View {
     @ObservedObject var viewModel: SettingsViewModel
+    @State var currentlyEditing: String?
+    @State var selection = Set<String>()
+    @State var editText: String = ""
+    @FocusState var editFocus: Bool
     
     var body: some View {
         VStack {
-            List(viewModel.runnerUUIDs, id: \.self) { uuid in
-                Text(viewModel.getRunnerName(uuid))
+            List(viewModel.runners, id: \.self, selection: $selection) { name in
+                HStack {
+                    if currentlyEditing == name {
+                        TextField("", text: $editText)
+                            .onSubmit {
+                                viewModel.renameRunner(oldName: name, newName: editText)
+                                currentlyEditing = nil
+                            }
+                            .focused($editFocus)
+                            .task {
+                                editFocus = true
+                                selection.removeAll()
+                                selection.insert(name)
+                            }
+                    } else {
+                        Text(name)
+                            .onTapGesture(count: 2, perform: {
+                                DispatchQueue.main.async {
+                                    editText = name
+                                    currentlyEditing = name
+                                }
+                            })
+                    }
+                    Spacer()
+                }
+
             }
             ListEditView(plus: {
                 viewModel.createRunner("runner")
             }, minus: {
-                
+                viewModel.deleteRunners(selection)
             })
         }
     }
--- Lee/Settings/SettingsViewModel.swift
+++ Lee/Settings/SettingsViewModel.swift
@@ -11,67 +11,83 @@ import SwiftUI
 class SettingsViewModel: ObservableObject {
     private let settingsStore = UserDefaults(suiteName: "settings")!
     
-    @Published public var runnerUUIDs: [UUID] = []
+    @Published public var runners: [String] = []
     
     public init() {
-        runnerUUIDs = getRunnerUUIDs()
-    }
-    
-    public func getRunnerUUIDs() -> [UUID] {
-        // Check for saved list of UUIDs
-        if let uuids = settingsStore.stringArray(forKey: "runners") {
-            // Convert strings to UUIDs
-            return uuids.map({ uuid in
-                UUID(uuidString: uuid)!
-            })
+        if let runners = settingsStore.stringArray(forKey: "__runners__") {
+            self.runners = runners
+        } else {
+            settingsStore.set([], forKey: "__runners__")
         }
-        
-        // Set default and return empty
-        settingsStore.set([], forKey: "runners")
-        return []
-    }
-    
-    public func getRunnerName(_ uuid: UUID) -> String {
-        let runner = lookupUUID(uuid)!
-        
-        // First item in runner string array is name
-        return runner[0]
     }
     
     public func createRunner(_ name: String) {
-        // Generate UUID for new runner
-        let uuid = getNewUUID()
-        
-        // Make string array to be saved for runner
-        let runner = [name]
-        
-        // Save new runner
-        saveUUID(uuid, value: runner)
-        runnerUUIDs.append(uuid)
-        saveRunnerUUIDs()
-    }
-    
-    private func lookupUUID(_ uuid: UUID) -> [String]? {
-        return settingsStore.stringArray(forKey: uuid.uuidString)
-    }
-    
-    private func saveUUID(_ uuid: UUID, value: [String]) {
-        settingsStore.set(value, forKey: uuid.uuidString)
-    }
-    
-    private func getNewUUID() -> UUID {
-        // Generate random UUID
-        var uuid = UUID()
-        
-        // Check uniqueness
-        while lookupUUID(uuid) != nil {
-            uuid = UUID()
+        // Ensure name is valid
+        var name = name
+        while !validateRunnerName(name) {
+            name.append("-new")
         }
         
-        return uuid
+        // Save new runner
+        runners.append(name)
+        saveRunners()
+        settingsStore.set([:], forKey: name)
     }
     
-    private func saveRunnerUUIDs() {
-        settingsStore.set(runnerUUIDs, forKey: "runners")
+    public func renameRunner(oldName: String, newName: String) {
+        // Ensure new name is valid
+        var name = newName
+        while !validateRunnerName(name) {
+            name.append("-new")
+        }
+        
+        // Lookup index of old name
+        if let index = runners.firstIndex(of: oldName) {
+            runners[index] = name
+            saveRunners()
+        } else {
+            print("Cannot rename runner, no runner with name '\(oldName)' exists")
+            return
+        }
+        
+        // Move runner entry
+        var newEntry: [String: String] = [:]
+        if let runnerEntry = settingsStore.dictionary(forKey: oldName) {
+            newEntry = (runnerEntry as? [String: String])!
+            settingsStore.removeObject(forKey: oldName)
+        }
+        
+        settingsStore.set(newEntry, forKey: name)
+    }
+    
+    public func deleteRunners(_ names: Set<String>) {
+        // Delete runner entries
+        for name in names {
+            settingsStore.removeObject(forKey: name)
+        }
+        
+        // Remove from runner list
+        runners.removeAll(where: { name in
+            names.contains(name)
+        })
+    }
+    
+    private func validateRunnerName(_ name: String) -> Bool {
+        // Can't be empty
+        if name.isEmpty {
+            return false
+        } else if name == "__runners__" {
+            // Reserved key
+            return false
+        } else if settingsStore.object(forKey: name) != nil {
+            // Key already exists
+            return false
+        }
+        
+        return true
+    }
+    
+    private func saveRunners() {
+        settingsStore.set(runners, forKey: "__runners__")
     }
 }
